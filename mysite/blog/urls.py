9da77bde1d106df1a14af215ce34cd8efa69670c
kadzima <pascal.german@yandex.ru>
2021-05-31 23:22:43 +0600

    My first django app, first commit

---
+++ mysite/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
+        r'(?P<post>[-\w]+)/$',
+        views.post_detail,
+        name='post_detail'),
+]
